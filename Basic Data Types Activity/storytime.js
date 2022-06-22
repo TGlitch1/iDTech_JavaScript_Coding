592687f753c43f9fa25ebeb1f27fe9ffc415f02c
TGlitch <oluwatoniafo1@gmail.com>
2022-06-22 11:39:54 -0700

    story time

---
+++ Basic Data Types Activity/storytime.js
@@ -0,0 +1,31 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings = "Welcome to my World";
+let name = 'Toni';
+let title = 'Warlord';
+let worldName = "Arsenal";
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 20;
+let year = 1993;
+let century = 15;
+// TODO: Create 1-Array variable to show a collection of items your character might
+// YOUR CODE GOES HERE
+let BattleWeapons = ['shotgun','grenade','laser rifle','knife','sniper'];
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let bestWeapon = true;
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greetings + "My name is" + name +"."); 
+console.log("I'm the " + title + " of this game");
+console.log("You have entered a different dimension, the " + worldName + " battleground");
+console.log(" After entering this dimension time changes");
+console.log("And that mean I'm " + age + " years old.");
+console.log("You are also in the year " + year + " in the " + century + " century" + "\n");
+console.log("I rule this world so i can use a variety of weapon to take you down sodont try anything funny");
+console.log("Weapons such as " + BattleWeapons);
+console.log("Invader: Do you have a favorite weapon?");
+console.log("Me: " + bestWeapon + ", my favorite weapon is the " + BattleWeapons[4]+ "." + "\n");
+console.log("You are to find weapons to use to defeat me in order to go back to your dimension >:)");
+console.log("You are free to room around but remember time is ticking...");
